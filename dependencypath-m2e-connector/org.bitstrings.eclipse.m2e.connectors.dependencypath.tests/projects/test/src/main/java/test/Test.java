6b71cd0bbb28ec80fb4902d0068fa675ec694e9e
Pino Silvaggio <pino.silvaggio@gmail.com>
2011-11-23 02:57:54 -0500

    new dependencypath connector

---
+++ dependencypath-m2e-connector/org.bitstrings.eclipse.m2e.connectors.dependencypath.tests/projects/test/src/main/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import noNamespace.Items;
+
+public class Test
+{
+    public Test()
+    {
+        Items items = Items.Factory.newInstance();
+    }
+}
---
+++ dependencypath-m2e-connector/org.bitstrings.eclipse.m2e.connectors.dependencypath/src/org/bitstrings/eclipse/m2e/connectors/dependencypath/DependencyPathProjectConfigurator.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *    Copyright (c) 2011 bitstrings.org - Pino Silvaggio
+ *
+ *    All rights reserved. This program and the accompanying materials
+ *    are made available under the terms of the Eclipse Public License v1.0
+ *    which accompanies this distribution, and is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+package org.bitstrings.eclipse.m2e.connectors.dependencypath;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+
+public class DependencyPathProjectConfigurator extends AbstractJavaProjectConfigurator
+{
+    @Override
+    public AbstractBuildParticipant getBuildParticipant(
+                    IMavenProjectFacade projectFacade,
+                    MojoExecution execution,
+                    IPluginExecutionMetadata executionMetadata)
+    {
+        return null;
+    }
+}
